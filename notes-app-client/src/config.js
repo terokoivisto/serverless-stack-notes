4d66105794a9b7fad5f502fc285cc99cfe5a5e4b
Tero <tero.koivisto@aalto.fi>
2020-05-25 22:35:55 +0300

    React app setup

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-north-1",
+        BUCKET: "haxwel-notes-uploads"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://idxfu854jd.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_Sv8jEUDbS",
+        APP_CLIENT_ID: "5f9mbp3ufvkcmmnkn7ar0ua1oa",
+        IDENTITY_POOL_ID: "eu-west-1:e31c046a-15b5-42ba-b880-b31afcb661c8"
+    }
+};
--- notes-app-client/src/index.js
+++ notes-app-client/src/index.js
@@ -3,12 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Amplify} from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID,
+        userPoolWebClientId: config.cognito.USER_POOL_ID
+    },
+    Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+        endpoints: [
+            {
+                name: "notes",
+                region: config.apiGateway.REGION,
+                endpoint: config.apiGateway.URL
+            },
+        ]
+    }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <App/>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
